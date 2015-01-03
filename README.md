9d70b77dcaf7fabfcb4d1c21678e4a4a65669f78
Travis Wachendorf <travis@mgnifiedonline.com>
2015-01-03 11:29:43 -0700

    Version 0.2 push

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ prettyphoto-media-pinterest
 ===========================
 
 Wordpress Plugin to add a Pinterest "Pin It" Button to prettyPhoto Media.
+
+01-2015
+
+Updated by removing pinterest element upon image click. Previously was adding button after button as you arrowed through image.
--- prettyphoto-media-pinterest.php
+++ prettyphoto-media-pinterest.php
@@ -3,12 +3,20 @@
  * Plugin Name: prettyPhoto Media Pinterest
  * Depends: prettyPhoto Media
  * Plugin URI: http://github.com/jmcleod/prettyphoto-media-pinterest
+ * Fork URI: https://github.com/Lawdawg/prettyphoto-media-pinterest
  * Description: Adds a Pinterest "Pin It" Button to prettyPhoto.
- * Version: 0.1
+ * Version: 0.2
  * Author: James McLeod
  */
+
+// initialize
 add_action('init', 'ppm_pinterest_init');
 
+
+/**
+ * Intialize function
+ * @return function adds scripts to header
+ */
 function ppm_pinterest_init() {
 	define('PPM_PINTEREST_URI', trailingslashit( plugin_dir_url( __FILE__ ) ) );
 	define('PPM_PINTEREST_VERSION', '0.1' );
@@ -20,25 +28,34 @@ function ppm_pinterest_init() {
 	}	
 }
 
+
+/**
+ * Outputs footer JavaScript
+ * @return string js code
+ */
 function ppm_pinterest_footer_script() {
 	$out = '<script>' . "\n";
-	$out .= 'jQuery(function($) {' . "\n";	
-	$out .= '  $(document).bind(\'DOMNodeInserted\', function(event) {' . "\n";
-	$out .= '    if (window.settings && !window.settings.changepicturecallbackupdated) {' . "\n";
-	$out .= '      window.settings.changepicturecallback = add_pinterest_pin_it_button' . "\n";
-	$out .= '      window.settings.changepicturecallbackupdated = true;' . "\n"; 
-	$out .= '    }' . "\n";
-	$out .= '  });' . "\n";
-	$out .= '});' . "\n";
-	$out .= "\n";
-	$out .= 'function add_pinterest_pin_it_button() {' . "\n";
-	$out .= '  var i = jQuery(\'.pp_gallery\').find(\'li\').index(jQuery(\'.selected\'));' . "\n";
-	$out .= '  var m = jQuery(\'#fullResImage\').attr(\'src\');' . "\n";
-	$out .= '  var o = jQuery(\'#fullResImage\').attr(\'alt\');' . "\n";
-	$out .= '  jQuery(\'.pp_social\').append(\'<div class="pinterest"><a target="_blank" href="http://pinterest.com/pin/create/button/?url=\' + encodeURI(location.href.replace(location.hash,"")) + \'&media=\' + encodeURI(m) + \'&description=\' + encodeURI(o) + \'" class="pin-it-button" count-layout="none"><img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" /></a></div>\');' . "\n";
-	$out .= '}' . "\n";
+
+		$out .= 'jQuery(function($) {' . "\n";	
+		$out .= '  $(document).bind(\'DOMNodeInserted\', function(event) {' . "\n";
+		$out .= '    if (window.settings && !window.settings.changepicturecallbackupdated) {' . "\n";
+		$out .= '      window.settings.changepicturecallback = add_pinterest_pin_it_button' . "\n";
+		$out .= '      window.settings.changepicturecallbackupdated = true;' . "\n"; 
+		$out .= '    }' . "\n";
+		$out .= '  });' . "\n";
+		$out .= '});' . "\n";
+		$out .= "\n";
+
+		$out .= 'function add_pinterest_pin_it_button() {' . "\n";
+		$out .= '  var i = jQuery(\'.pp_gallery\').find(\'li\').index(jQuery(\'.selected\'));' . "\n";
+		$out .= '  var m = jQuery(\'#fullResImage\').attr(\'src\');'."\n";
+		$out .= ' var ol = window.location.hostname + window.location.pathname;'."\n";
+	        	$out .= '  jQuery(\'.pinterest\').remove();' . "\n";
+		$out .= '  jQuery(\'.pp_social\').append(\'<div class="pinterest"><a href="http://pinterest.com/pin/create/button/?url=\' + encodeURI(ol) + \'&media=\' + encodeURI(m) + \'&description=\' + encodeURI(\'Wedding Photo Idea found on Eyeshot Photography\') + \'" class="pin-it-button" count-layout="none" target="_blank"><img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" /></a></div>\');' . "\n";
+		$out .= '}' . "\n";
+
 	$out .= '</script>' . "\n";
-	
+
 	echo $out;
 }
 
